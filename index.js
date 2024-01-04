c71eaac0eaebe751854f746c66aedd6cfe4df1a2
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-01-04 16:38:25 -0300

    docs: request movidas para routes

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const express = require('express')
-const posts = require('./model/posts')
+const apiRouter = require('./routes/api')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
-app.get('/all', (req, res) => {
-    console.log(posts.getAll())
-    res.json(JSON.stringify(posts.getAll()))
-})
-
-app.post('/new', express.json(), (req, res) => {
-    const title = req.body.title
-    const discription = req.body.discription
-
-    posts.newPost(title, discription)
-
-    res.send('post sent')
-})
-
-app.delete('/delete', express.json(), (req, res) => {
-    const id = req.body.id
-
-    posts.deletePost(id)
-
-    res.send('post deleted')
-})
+app.use('/api', apiRouter)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
     console.clear()
---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const posts = require('../model/posts')
+
+const router = express.Router()
+
+router.get('/all', (req, res) => {
+    console.log(posts.getAll())
+    res.json(JSON.stringify(posts.getAll()))
+})
+
+router.post('/new', express.json(), (req, res) => {
+    const title = req.body.title
+    const discription = req.body.discription
+
+    posts.newPost(title, discription)
+
+    res.send('post sent')
+})
+
+router.delete('/delete', express.json(), (req, res) => {
+    const id = req.body.id
+
+    posts.deletePost(id)
+
+    res.send('post deleted')
+})
+
+module.exports = router
