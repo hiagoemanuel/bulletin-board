70a0b292affb8eb420ca0b5d2549aca2845eb14b
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-01-04 14:57:45 -0300

    feat: obj criado para gerenciar posts
    feat: func deletePost criada

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express')
+const posts = require('./model/posts')
 
 const app = express()
 const port = 3000
 
-const posts = [
-    {
-        id: 'SDawWOnmWR',
-        title: 'board test',
-        discription: 'this is the dicription'
-    }
-]
-
 app.get('/all', (req, res) => {
-    res.json(JSON.stringify(posts))
+    console.log(posts.getAll())
+    res.json(JSON.stringify(posts.getAll()))
 })
 
-app.post('/new', (req, res) => {
+app.post('/new', express.json(), (req, res) => {
+    const title = req.body.title
+    const discription = req.body.discription
 
+    posts.newPost(title, discription)
+
+    res.send('post sent')
+})
+
+app.delete('/delete', express.json(), (req, res) => {
+    const id = req.body.id
+
+    posts.deletePost(id)
+
+    res.send('post deleted')
 })
 
 app.listen(port, () => {
---
+++ model/posts.js
@@ -0,0 +1,22 @@
+module.exports = {
+    posts: [
+        {
+            id: 'SDawWOnmWR',
+            title: 'board test',
+            discription: 'this is the dicription'
+
+        }
+    ],
+    getAll() {
+        return this.posts
+    },
+    newPost(title, discription) {
+        return this.posts.push({ id: generateId(), title, discription })
+    },
+    deletePost(id) {
+        const newPosts = this.posts.filter(p => p.id !== id)
+        this.posts = newPosts
+    }
+}
+
+const generateId = () => Math.random().toString(36).substring(2, 9)
